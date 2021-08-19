36f1939601af158bd9a0bdb2254fb95f33c8433e
virnacabaguing1007 <86231992+virnacabaguing1007@users.noreply.github.com>
2021-08-20 01:27:46 +0800

    try1

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'my-first-app'`, () => {
+  it(`should have as title 'Virna App'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-first-app');
+    expect(app.title).toEqual('Virna App');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('my-first-app app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Virna App app is running!');
   });
 });
